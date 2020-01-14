bfb97444aa0b86056a1f4c796dc832839617c514
Alex Cicioc <alexandru.cicioc@softvision.com>
2020-01-14 10:43:19 +0200

    Add collision detection

--- spaceship/game.js
+++ spaceship/game.js
@@ -14,36 +14,63 @@ document.addEventListener('mousemove', (event) => {
     mouse.y = event.y;
 });
 
-const objects = [
-    new Ship(mouse),
-    // new Invader(50, 100),
-    // new Invader(50, 200),
-    // new Invader(200, 100),
-    // new Invader(200, 200),
-    // new Invader(300, 60),
-    // new Invader(50, 300),
-];
+const objects = {
+    ship: new Ship(mouse),
+    shots: [],
+    invaders: []
+};
+
 
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 4; j++) {
-        objects.push(new Invader(i * 100, j * 100));
+        objects.invaders.push(new Invader(i * 100, j * 100));
     }
+}
+
+function isShotColliding(shot, invader) {
+    return Math.abs(shot.x - invader.x) < 60 && Math.abs(shot.y - invader.y) < 60;
+}
+
+function isInvaderColliding(invader, ship) {
+    return Math.abs(ship.x - invader.x) < 60 && Math.abs(ship.y - invader.y) < 60;
 }
 
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    objects.forEach((object, index) => {
-        if (object instanceof Shot && object.y <= 0) {
-            delete objects[index];
+    objects.invaders.forEach((invader, index) => {
+        invader.move();
+        if (isInvaderColliding(invader, objects.ship)) {
+            alert("You're a loser");
+        }
+    });
+
+    objects.shots.forEach((shot, shotIndex) => {
+        if (shot.y <= 0) {
+            delete objects.shots[shotIndex];
             return;
         }
-        object.move();
-    })
+
+        let shotDeleted = false;
+        objects.invaders.forEach((invader, invaderIndex) => {
+            if (isShotColliding(shot, invader)) {
+                console.log('intra');
+                delete objects.shots[shotIndex];
+                delete objects.invaders[invaderIndex];
+                shotDeleted = true;
+            }
+        });
+
+        if (!shotDeleted) {
+            shot.move();
+        }
+    });
+
+    objects.ship.move();
 }
 
 animate();
 
 document.addEventListener('click', (event) => {
-    objects.push(new Shot(mouse.x, mouse.y));
+    objects.shots.push(new Shot(mouse.x, mouse.y));
 });
